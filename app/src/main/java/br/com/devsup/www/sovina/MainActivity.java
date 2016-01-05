af9a71fe6ac34df701560c6eb5f71a329bcd92de
rov <valadares.thel@gmail.com>
2016-01-05 20:36:30 -0200

    Adding editText empty validation

--- app/src/main/java/br/com/devsup/www/sovina/MainActivity.java
+++ app/src/main/java/br/com/devsup/www/sovina/MainActivity.java
@@ -1,7 +1,7 @@
 package br.com.devsup.www.sovina;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -31,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickFloatButton (View view) {
+        if(!fieldVerification()) {
+            return;
+        }
+
         final double quantityProduct01 = Double.valueOf(editTextQuantityProduct01.getText().toString());
         final double valueProduct01 = Double.valueOf(editTextValueProduct01.getText().toString());
         final double quantityProduct02 = Double.valueOf(editTextQuantityProduct02.getText().toString());
@@ -50,6 +54,41 @@ public class MainActivity extends AppCompatActivity {
         Snackbar.make(view, resultText, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
     }
+
+    private boolean fieldVerification() {
+        boolean result = true;
+
+        if(editTextQuantityProduct01.getText().toString().isEmpty()) {
+            modalAlert(getString(R.string.field_validation_product_01));
+            editTextQuantityProduct01.requestFocus();
+            result = false;
+        } else if(editTextValueProduct01.getText().toString().isEmpty()) {
+            modalAlert(getString(R.string.field_validation_product_01));
+            editTextValueProduct01.requestFocus();
+            result = false;
+
+        } else if(editTextQuantityProduct02.getText().toString().isEmpty()) {
+            modalAlert(getString(R.string.field_validation_product_02));
+            editTextQuantityProduct02.requestFocus();
+            result = false;
+
+        } else if(editTextValueProduct02.getText().toString().isEmpty()) {
+            modalAlert(getString(R.string.field_validation_value_product_02));
+            editTextValueProduct02.requestFocus();
+            result = false;
+        }
+
+        return result;
+    }
+
+    private void modalAlert(String string) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle(getString(R.string.field_validation_title));
+        alert.setMessage(string);
+        alert.show();
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
